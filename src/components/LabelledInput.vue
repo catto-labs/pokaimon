6b55f7b3d1acb61552c263e7aa260a262c4edddf
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-12 18:25:41 +0200

    feat: add `LabelledInput` component

---
+++ src/components/LabelledInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  placeholder: String,
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-3">
+    <label class="text-lg text-note">{{ props.label }}</label>
+    <input
+      class="w-full rounded-2xl border border-grey-700 bg-grey-900 py-1.5 px-4 text-grey-200 outline-none transition hover:bg-opacity-80 focus:border-grey-500 focus:bg-opacity-80"
+      :placeholder="props.placeholder"
+    />
+  </div>
+</template>
