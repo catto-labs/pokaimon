e8ed48e829583e31e629c89becd078e31c6b08c0
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-12 18:29:06 +0200

    refactor: move font package to deps and not devDeps

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import App from "./App.vue";
 
 import "@/assets/css/main.css";
 import "@fontsource/raleway/400.css";
