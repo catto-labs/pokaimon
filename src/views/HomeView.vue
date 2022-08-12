619cfb530d42b9a7a59733993af47102124e7f36
Trobo <57040351+trobonox@users.noreply.github.com>
2022-08-12 21:46:20 +0300

    feat: start populating home page with content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div>
+    <Navbar class="fixed top-0 w-full" />
+    <main class="px-8 pt-16">
+      <h1 class="mt-32 text-6xl font-bold">Embark the adventure.</h1>
+      <p class="max-w-xl">
+        A simple game inspired by Pokemon, where you can have 1v1 fights and
+        collect character cards, items and ingame currency in the setting of the
+        Genshin Impact world.
+      </p>
+    </main>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+</script>
