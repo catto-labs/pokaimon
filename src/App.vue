65d669c2267b6f7c02025f5669ec4c4b6fd5dabc
Trobo <57040351+trobonox@users.noreply.github.com>
2022-08-12 18:27:16 +0300

    Basic styling

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,16 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
-  <h1 class="text-success">Poggers</h1>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Raleway, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  color: #e5e7eb;
+  background-color: #111827;
+  min-height: 100vh;
+  min-width: 100%;
 }
 </style>
