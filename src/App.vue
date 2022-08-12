a88858ed87ab927b90be12394f19e446ab847832
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-12 19:40:46 +0200

    fix: use `#app` styling on HTML `body`

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@
 
 <style>
 #app {
-  font-family: Raleway, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #e5e7eb;
-  background-color: #111827;
   min-height: 100vh;
   min-width: 100%;
 }
