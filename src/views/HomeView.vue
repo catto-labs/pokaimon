51042febd5453e0413bd8a7cd369edd0e4a78c76
Trobo <57040351+trobonox@users.noreply.github.com>
2022-08-12 21:52:43 +0300

    feat: add CTA and secondary button on main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,16 @@
         collect character cards, items and ingame currency in the setting of the
         Genshin Impact world.
       </p>
+      <div class="mt-4 flex flex-row gap-4">
+        <button class="rounded-2xl bg-purple px-8 py-2 text-xl font-bold">
+          Play Now
+        </button>
+        <button
+          class="rounded-2xl border border-blue px-8 py-2 text-xl font-bold hover:bg-blue"
+        >
+          Learn More
+        </button>
+      </div>
     </main>
   </div>
 </template>
