bb73a166070122d7f6e960b2ec0889fc9a2d4eb5
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-12 18:26:14 +0200

    feat: add login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex h-screen w-screen items-center justify-center bg-grey-900">
+    <div class="w-full max-w-md rounded-xl bg-black p-11">
+      <h1 class="mb-2 text-4xl font-bold text-head">Welcome back!</h1>
+
+      <form @submit="handleSubmit" class="pt-4">
+        <LabelledInput
+          label="Email address"
+          placeholder="traveler@pokaimon.ml"
+        />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LabelledInput from "@/components/LabelledInput.vue";
+
+const handleSubmit = (e: Event) => {
+  e.preventDefault();
+
+  console.log("hello");
+};
+</script>
