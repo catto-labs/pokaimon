423540001b30b928faa37cb7e6a9769b1ff88052
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-13 00:34:25 +0200

    feat: use pages directory instead of manual router

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import "@fontsource/raleway/600.css";
 import "@/assets/css/main.css";
 
 import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
 
-import router from "@/router/index";
 import App from "@/App.vue";
+import routes from "virtual:generated-pages";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
 createApp(App).use(router).mount("#app");
