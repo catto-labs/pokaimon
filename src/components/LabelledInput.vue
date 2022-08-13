a32ce0dfb9c36b0266cd3506ce668c37e6690c56
jason <pnxl@pm.me>
2022-08-13 12:29:26 +0700

    feat: changed spacing and text colour

--- src/components/LabelledInput.vue
+++ src/components/LabelledInput.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col gap-2.5">
+  <div class="flex flex-col space-y-2">
     <label class="text-lg text-note">{{ props.label }}</label>
     <input
       :type="props.type"
       :placeholder="props.placeholder"
       :value="modelValue"
       @input="updateValue"
-      class="w-full rounded-2xl border-2 border-grey-700 bg-grey-900 py-1.5 px-4 text-head outline-none transition placeholder:text-grey-600 hover:bg-opacity-80 focus:border-grey-500 focus:bg-opacity-80"
+      class="w-full rounded-2xl border-2 border-grey-700 bg-grey-900 py-2 px-4 text-body outline-none transition placeholder:text-grey-600 hover:bg-opacity-80 focus:border-grey-500 focus:bg-opacity-80"
     />
   </div>
 </template>
