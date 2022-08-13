deb15c88f8fad4914b347aba504f3897ef0323d3
Trobo <57040351+trobonox@users.noreply.github.com>
2022-08-13 11:19:12 +0300

    feat: add signout page

---
+++ src/pages/signout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><h1>Signing you out...</h1></div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount } from "vue";
+
+import { supabase } from "@/utils/supabase";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+onBeforeMount(async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) alert(error);
+  router.push("/");
+});
+</script>
