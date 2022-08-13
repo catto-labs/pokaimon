e404d7ba6262b0ec068959a33c0c14748ab57617
Trobo <57040351+trobonox@users.noreply.github.com>
2022-08-13 11:18:57 +0300

    feat: add placeholder onboarding page

---
+++ src/pages/onboarding.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Poggers</div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount } from "vue";
+import { useRouter } from "vue-router";
+
+import { supabase } from "@/utils/supabase";
+import { store } from "@/utils/store";
+
+const router = useRouter();
+
+onBeforeMount(() => {
+  if (supabase.auth.session() === null) router.push("/signup");
+
+  store.showOnboarding = false;
+});
+</script>
