38dabef45f38df301382eea38c84ce2bec02c6b0
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-13 20:39:04 +0200

    styles: navbar on very small devices

--- src/components/pages/global/Navbar.vue
+++ src/components/pages/global/Navbar.vue
@@ -29,7 +29,7 @@
 
         <a
           href="/game"
-          class="m-0 inline-block transform rounded-lg bg-brand-main py-1 px-2.5 text-center filter duration-300 ease-in-out hover:-translate-y-0.5 hover:font-bold md:w-auto"
+          class="m-0 hidden transform rounded-lg bg-brand-main py-1 px-2.5 text-center filter duration-300 ease-in-out hover:-translate-y-0.5 hover:font-bold sm:inline-block md:w-auto"
         >
           Play Now
         </a>
@@ -42,12 +42,19 @@
 
     <nav
       ref="mobileMenu"
-      class="absolute top-4 left-0 right-0 mx-8 mt-14 hidden items-center rounded-xl border border-grey-700 bg-grey-800 bg-opacity-60 py-4 filter backdrop-blur-md md:top-auto md:left-auto md:right-4 md:mt-0 md:hidden md:space-y-0 md:space-x-8 md:border-none md:px-0 md:py-0 md:pl-4"
+      class="absolute top-4 left-0 right-0 mx-8 mt-14 hidden items-center rounded-xl border border-grey-700 bg-grey-800 bg-opacity-60 p-4 filter backdrop-blur-md md:top-auto md:left-auto md:right-4 md:mt-0 md:hidden md:space-y-0 md:space-x-8 md:border-none md:px-0 md:py-0 md:pl-4"
     >
       <NavbarItem href="/cards" text="Cards" />
       <NavbarItem href="/sets" text="Card Sets" />
       <NavbarItem href="/community" text="Community" />
       <NavbarItem href="/news" text="News" />
+
+      <a
+        href="/game"
+        class="mt-4 inline-block w-full transform rounded-lg bg-brand-main py-1.5 px-4 filter duration-300 ease-in-out hover:-translate-y-0.5 hover:font-bold sm:hidden md:w-auto"
+      >
+        Play Now
+      </a>
     </nav>
   </div>
 </template>
--- src/components/pages/global/NavbarItem.vue
+++ src/components/pages/global/NavbarItem.vue
@@ -1,9 +1,9 @@
 <template>
   <a
     :href="props.href"
-    class="m-0 block w-full py-2 transition duration-300 ease-in-out hover:bg-white hover:bg-opacity-10 hover:text-brand-second md:w-auto md:py-0 md:hover:bg-opacity-0"
+    class="m-0 mb-1 block w-full rounded-xl py-2 px-4 transition duration-300 ease-in-out hover:bg-white hover:bg-opacity-10 hover:text-brand-second md:w-auto md:py-0 md:hover:bg-opacity-0"
   >
-    <p class="mx-4 md:mx-0">{{ props.text }}</p>
+    {{ props.text }}
   </a>
 </template>
 
