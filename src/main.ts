b842269afdb126a59dbb31149426859718fac5f4
jason <pnxl@pm.me>
2022-08-13 11:07:35 +0700

    style: add comments for css imports

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+// #region css imports
+// raleway font
 import "@fontsource/raleway/400.css";
 import "@fontsource/raleway/600.css";
+
+// styles
 import "@/assets/css/main.css";
 import "@/assets/css/text.css";
+// #endregion css imports
 
 import { createApp } from "vue";
 import { createWebHistory, createRouter } from "vue-router";
