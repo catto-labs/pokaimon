000f5f9b8e0fa28d27449a0e5f8c435b90d89883
jason <pnxl@pm.me>
2022-08-13 10:04:08 +0700

    feat: navbar cleanup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="p-2">
+  <div class="py-4 px-8">
     <header
       class="flex w-full flex-row justify-between rounded-xl bg-grey-800 bg-opacity-60 py-2 px-4 filter backdrop-blur-md"
     >
-      <div class="flex flex-row items-center gap-4">
-        <img
-          src="../assets/logo.png"
-          alt="Pokaimon logo"
-          class="h-8"
-          id="header-logo"
-        />
-        <label for="header-logo" class="text-lg font-bold">Pokaimon</label>
-      </div>
-      <nav class="flex flex-row items-center gap-4">
+      <a href="/" class="cursor-pointer">
+        <div class="flex flex-row items-center gap-4">
+          <img
+            src="../assets/logo.png"
+            alt="Pokaimon logo"
+            class="my-auto h-8"
+            id="header-logo"
+          />
+          <label
+            for="header-logo"
+            class="my-auto cursor-pointer text-lg font-bold"
+            >Pokaimon</label
+          >
+        </div>
+      </a>
+      <nav class="flex flex-row items-center space-x-8">
         <a href="/cards" class="hover:text-purple">Cards</a>
         <a href="/sets" class="hover:text-purple">Card Sets</a>
         <a href="/community" class="hover:text-purple">Community</a>
         <a href="/news" class="hover:text-purple">News</a>
         <a
           href="/game"
-          class="rounded-md bg-purple p-1 filter hover:brightness-75"
+          class="rounded-md bg-purple px-2 py-1 filter hover:brightness-75"
           >Play Now</a
         >
       </nav>
