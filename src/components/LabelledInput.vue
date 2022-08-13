e5295b9787cd2ddc10624278408a6ec1bf013c68
Trobo <57040351+trobonox@users.noreply.github.com>
2022-08-13 18:09:11 +0300

    feat: add prop for regex pattern input matching

--- src/components/LabelledInput.vue
+++ src/components/LabelledInput.vue
@@ -5,6 +5,7 @@
       :type="props.type"
       :placeholder="props.placeholder"
       :value="modelValue"
+      :pattern="pattern"
       @input="updateValue"
       class="w-full rounded-2xl border-2 border-grey-700 bg-grey-900 py-2 px-4 text-body outline-none transition placeholder:text-grey-600 hover:bg-opacity-80 focus:border-grey-500 focus:bg-opacity-80"
     />
@@ -17,6 +18,7 @@ const props = defineProps<{
   label: string;
   modelValue?: string;
   placeholder?: string;
+  pattern?: string;
 }>();
 
 const emit = defineEmits(["update:modelValue"]);
