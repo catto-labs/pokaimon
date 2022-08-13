685812b02cf48657bc7d8f5e02e51e455ce42e9a
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-13 17:53:42 +0200

    feat: add navbar class for mobile

--- src/components/pages/global/Navbar.vue
+++ src/components/pages/global/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="py-4 px-8">
     <header
-      class="flex w-full flex-row justify-between rounded-xl border border-grey-700 bg-grey-800 bg-opacity-60 py-2 px-4 filter backdrop-blur-md"
+      class="h-13 flex w-full flex-row items-center justify-between rounded-xl border border-grey-700 bg-grey-800 bg-opacity-60 py-2 px-4 filter backdrop-blur-md"
     >
       <a href="/" class="cursor-pointer">
         <div class="flex flex-row items-center gap-4">
           <img
-            src="../../../assets/logo.png"
-            alt="Pokaimon logo"
+            src="@/assets/logo.png"
+            alt="Pokaimon Logo"
             class="my-auto h-8"
             id="header-logo"
           />
@@ -18,33 +18,25 @@
           >
         </div>
       </a>
-      <nav class="flex flex-row items-center space-x-8">
-        <a
-          href="/cards"
-          class="transition duration-300 ease-in-out hover:text-brand-second"
-          >Cards</a
-        >
-        <a
-          href="/sets"
-          class="transition duration-300 ease-in-out hover:text-brand-second"
-          >Card Sets</a
-        >
-        <a
-          href="/community"
-          class="transition duration-300 ease-in-out hover:text-brand-second"
-          >Community</a
-        >
-        <a
-          href="/news"
-          class="transition duration-300 ease-in-out hover:text-brand-second"
-          >News</a
-        >
+      <nav
+        class="fixed top-0 left-0 right-0 mt-14 items-center space-y-2 rounded-xl border border-grey-700 bg-grey-800 bg-opacity-60 px-4 py-4 filter backdrop-blur-md md:top-auto md:left-auto md:right-4 md:mt-0 md:flex md:flex-row md:space-y-0 md:space-x-8 md:border-none md:px-0 md:py-0 md:pl-4"
+      >
+        <NavbarItem href="/cards" text="Cards" />
+        <NavbarItem href="/sets" text="Card Sets" />
+        <NavbarItem href="/community" text="Community" />
+        <NavbarItem href="/news" text="News" />
+
         <a
           href="/game"
-          class="transform rounded-lg bg-brand-main px-2 py-1 filter duration-300 ease-in-out hover:-translate-y-0.5 hover:font-bold"
-          >Play Now</a
+          class="m-0 inline-block transform rounded-lg bg-brand-main py-1 px-2.5 text-center filter duration-300 ease-in-out hover:-translate-y-0.5 hover:font-bold md:w-auto"
         >
+          Play Now
+        </a>
       </nav>
     </header>
   </div>
 </template>
+
+<script setup lang="ts">
+import NavbarItem from "./NavbarItem.vue";
+</script>
---
+++ src/components/pages/global/NavbarItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    :href="props.href"
+    class="m-0 block w-full py-2 transition duration-300 ease-in-out hover:text-brand-second md:w-auto md:py-0"
+  >
+    {{ props.text }}
+  </a>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  href: string;
+  text: string;
+}>();
+</script>
