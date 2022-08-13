3ee961b850d7f19507801919053a8b89648ecb4c
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-13 12:25:01 +0200

    fix: pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -33,23 +33,23 @@ export default defineConfig({
 
         icons: [
           {
-            src: "icons/icon-default.png",
+            src: "icons/pwa/pwa-icon.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icons/icon-default-large.png",
+            src: "icons/pwa/pwa-icon-large.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "icons/icon-default-maskable.png",
+            src: "icons/pwa/pwa-icon-maskable.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable",
           },
           {
-            src: "icons/icon-default-maskable-large.png",
+            src: "icons/pwa/pwa-icon-maskable-large.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
