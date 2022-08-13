5ded4ee6ae5a9ca4f304383fdf9434464b6458e9
jason <pnxl@pm.me>
2022-08-13 17:21:51 +0700

    feat(onboarding): username and tag selection

---
+++ src/pages/onboarding/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { onBeforeMount } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+onBeforeMount(() => {
+  router.push("/onboarding/user");
+});
+</script>
---
+++ src/pages/onboarding/user.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="bg-gray-900 flex flex-col items-center py-32">
+    <div class="text-center">
+      <h1 class="mb-2 text-4xl font-bold text-head">Let your name be known.</h1>
+      <h2 class="mb-2 text-xl text-body">What should we call you?</h2>
+    </div>
+    <form @submit="handleSubmit" class="flex flex-col gap-4 pt-16">
+      <div class="flex flex-row gap-8">
+        <LabelledInput
+          type="text"
+          v-model="state.username"
+          label="Username"
+          class="w-3/4"
+        />
+        <LabelledInput
+          type="text"
+          v-model="state.tag"
+          label="Tag"
+          class="w-1/4"
+        />
+      </div>
+      <button
+        type="submit"
+        class="mt-2 w-full rounded-2xl bg-grey-700 py-2 px-4 font-bold text-white transition duration-300 hover:-translate-y-0.5 hover:bg-grey-600"
+      >
+        Proceed
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, onBeforeMount } from "vue";
+import { useRouter } from "vue-router";
+
+import LabelledInput from "@/components/LabelledInput.vue";
+
+import { supabase } from "@/utils/supabase";
+import { store } from "@/utils/store";
+
+const router = useRouter();
+
+const state = reactive({
+  username: "",
+  tag: "",
+});
+
+const handleSubmit = (e: Event) => {
+  e.preventDefault();
+
+  if (state.username === "") return alert("Please enter a username");
+  if (state.tag === "") return alert("Please enter a tag");
+
+  // signUpWithEmail(state.email, state.password);
+  // something here idk what im doing tbh ~Pixel
+};
+
+onBeforeMount(() => {
+  if (supabase.auth.session() === null) router.push("/signup");
+
+  store.showOnboarding = false;
+});
+</script>
