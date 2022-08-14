13017572891c6fe414040c90251f3e264a770f6f
jason <pnxl@pm.me>
2022-08-14 11:08:15 +0700

    fix: hide underline on hover

--- src/components/pages/global/NavbarItem.vue
+++ src/components/pages/global/NavbarItem.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     :href="props.href"
-    class="m-0 mb-1 block w-full rounded-xl py-2 px-4 transition duration-300 ease-in-out hover:bg-white hover:bg-opacity-10 hover:text-brand-second md:mb-0 md:w-auto md:py-0 md:hover:bg-opacity-0"
+    class="m-0 mb-1 block w-full rounded-xl py-2 px-4 transition duration-300 ease-in-out hover:bg-white hover:bg-opacity-10 hover:text-brand-second hover:no-underline md:mb-0 md:w-auto md:py-0 md:hover:bg-opacity-0"
   >
     {{ props.text }}
   </a>
