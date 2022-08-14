ac7017ad231fc75fc55218aeeceea980c859472b
Jason <pnxl@pm.me>
2022-08-14 16:43:03 +0700

    feat: changed alt tags, added more roundness, fixed size

--- src/pages/forgot-password.vue
+++ src/pages/forgot-password.vue
@@ -1,12 +1,13 @@
 <template>
   <div
-    class="bg-gray-900 flex h-screen w-screen flex-col items-center justify-center px-6"
+    class="bg-gray-900 flex flex-col items-center justify-center space-y-8 px-6 py-8"
   >
-    <h1 class="mb-8 text-4xl font-bold">Oh hey, you found an easter egg!</h1>
+    <h1 class="text-4xl font-bold">Oh hey, you found an easter egg!</h1>
     <img
       src="../assets/forgot-password.jpg"
       alt="Forgot password megamind meme image"
-      class="rounded-md"
+      class="h-[28rem] rounded-xl"
     />
+    <p>Maybe use a password manager next time?</p>
   </div>
 </template>
--- src/pages/no-account.vue
+++ src/pages/no-account.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="bg-gray-900 flex h-screen w-screen flex-col items-center justify-center px-6"
+    class="bg-gray-900 flex flex-col items-center justify-center space-y-8 px-6 py-8"
   >
-    <h1 class="mb-8 text-4xl font-bold">Oh hey, you found an easter egg!</h1>
+    <h1 class="text-4xl font-bold">Oh hey, you found an easter egg!</h1>
     <img
       src="../assets/no-account.jpg"
-      alt="No account meme image"
-      class="rounded-md"
+      alt="No account megamind meme image"
+      class="h-[28rem] rounded-xl"
     />
   </div>
 </template>
