a5a0bab33712a7850fc94c02b1453870dbf205d3
Jason <pnxl@pm.me>
2022-08-14 16:39:56 +0700

    fix(accessibility): wrong alt description

--- src/pages/forgot-password.vue
+++ src/pages/forgot-password.vue
@@ -5,7 +5,7 @@
     <h1 class="mb-8 text-4xl font-bold">Oh hey, you found an easter egg!</h1>
     <img
       src="../assets/forgot-password.jpg"
-      alt="No account meme image"
+      alt="Forgot password megamind meme image"
       class="rounded-md"
     />
   </div>
