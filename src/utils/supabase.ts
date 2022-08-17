6c20e49db5f820d8714ebb3cd8611964b5d178ac
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-17 22:30:57 +0200

    fix: add typings for Database

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,9 +1,10 @@
+import type { Database } from "@/types/Database";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 /**
  * Get the `maps` bucket URL.
