3b412e63311a36b8eab5b3cae16d8eb87718d31c
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-18 14:05:36 +0200

    chore: test function

--- supabase/functions/create-game/index.ts
+++ supabase/functions/create-game/index.ts
@@ -1,6 +1,8 @@
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
+//
+//
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 import { supabaseClient } from "../_shared/supabaseClient.ts";
 import { corsHeaders } from "../_shared/cors.ts";
