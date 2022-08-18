a267e574eb3b0633d945752f13ad69281698b403
Trobo <57040351+trobonox@users.noreply.github.com>
2022-08-18 23:10:17 +0300

    feat: add plain inventory placeholder page

---
+++ src/pages/game/inventory.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="bg-gray-900 flex h-screen w-screen flex-col items-center justify-center"
+  >
+    <router-link
+      to="/game"
+      class="group absolute top-0 left-0 z-10 flex flex-row items-center gap-4 p-4 hover:no-underline"
+    >
+      <IconArrowLeft
+        class="text-grey-400 transition-colors group-hover:text-brand-second"
+      />
+      <span class="transition-colors group-hover:text-brand-second">
+        Go back
+      </span>
+    </router-link>
+    <h1>Inventory</h1>
+  </div>
+</template>
+
+<route>
+{
+  meta: {
+    requiresAuth: true,
+    checkForCompletedOnboarding: true
+  }
+}
+</route>
