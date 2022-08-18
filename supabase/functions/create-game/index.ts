3edef8fa3a83d7eabe1b8ca08a0558bd1ffed91e
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-18 13:53:22 +0200

    feat: add create-game logic

--- supabase/functions/create-game/index.ts
+++ supabase/functions/create-game/index.ts
@@ -1,20 +1,57 @@
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
-
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+import { supabaseClient } from "../_shared/supabaseClient.ts";
+import { corsHeaders } from "../_shared/cors.ts";
 
-serve(async (req) => {
-  const { player1, player2 } = await req.json();
+serve(async (req: Request) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === "OPTIONS") {
+    return new Response("ok", {
+      headers: corsHeaders,
+    });
+  }
 
-  const isOfflineFight = !player2;
+  try {
+    // Set the Auth context of the user that called the function.
+    // This way our row-level-security (RLS) policies are applied.
+    supabaseClient.auth.setAuth(
+      req.headers.get("Authorization").replace("Bearer ", "")
+    );
 
-  const data = {
-    success: true,
-    isOfflineFight,
-  };
+    const { player1, player2 } = await req.json();
 
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
-  });
+    const { data, error } = await supabaseClient
+      .from("games")
+      .insert([
+        {
+          player1,
+          player2,
+        },
+      ])
+      .select();
+
+    return new Response(JSON.stringify({ data, error }), {
+      headers: {
+        ...corsHeaders,
+        "Content-Type": "application/json",
+      },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        data: null,
+        error: error.message,
+      }),
+      {
+        headers: {
+          ...corsHeaders,
+          "Content-Type": "application/json",
+        },
+        status: 400,
+      }
+    );
+  }
 });
