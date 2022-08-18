5f224d8a979c411816b3d96d428e5b5972500e61
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-18 22:23:36 +0200

    chore: draft of custom typings for database

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 /**
  * Get the `maps` bucket URL.
@@ -12,3 +12,31 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey);
  */
 export const storedMapsUrl = (map_name: "teyvat") =>
   supabaseUrl.replace(/\/$/, "") + `/storage/v1/object/public/maps/${map_name}`;
+
+export const getCharacterInventory = async (id: number) => {
+  const getData = () =>
+    supabase.from("character_inventory").select(`*`).match({ id }).single();
+
+  type CharacterInventoryResponse = Awaited<ReturnType<typeof getData>>;
+  type CharacterInventoryResponseSuccess = Omit<
+    Database["public"]["Tables"]["character_inventory"]["Row"],
+    "base_character"
+  > & {
+    base_character: Omit<
+      Database["public"]["Tables"]["character_info"]["Row"],
+      "action_1" | "action_2" | "action_3" | "action_4"
+    > & {
+      action_1: Database["public"]["Tables"]["character_actions"]["Row"];
+      action_2: Database["public"]["Tables"]["character_actions"]["Row"];
+      action_3: Database["public"]["Tables"]["character_actions"]["Row"];
+      action_4: Database["public"]["Tables"]["character_actions"]["Row"];
+    };
+  };
+
+  const response = (await getData()) as {
+    error: CharacterInventoryResponse["error"];
+    data: CharacterInventoryResponseSuccess;
+  };
+
+  return response;
+};
