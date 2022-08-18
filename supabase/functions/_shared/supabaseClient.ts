826b982ab5c1fe4e9ccae2d5165069e559a5dbf7
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-18 15:00:55 +0200

    fix: use service role key for supabase functions

--- supabase/functions/_shared/supabaseClient.ts
+++ supabase/functions/_shared/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2.0.0-rc.3";
 export const supabaseClient = (authorization: string) =>
   createClient(
     Deno.env.get("SUPABASE_URL") ?? "",
-    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
     {
       auth: {
         autoRefreshToken: false,
