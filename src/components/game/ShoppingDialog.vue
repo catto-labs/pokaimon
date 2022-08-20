5a18eee2361c7c6f345136eed0fe24b2807f5905
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-20 16:40:40 +0200

    feat: add shopping dialog

---
+++ src/components/game/ShoppingDialog.vue
@@ -0,0 +1,65 @@
+<template>
+  <TransitionRoot appear :show="props.open" as="template">
+    <Dialog as="div" @close="props.closeShopDialog" class="relative z-50">
+      <TransitionChild
+        as="template"
+        enter="duration-200 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-100 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-grey-900 bg-opacity-75" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-200 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-xl transform overflow-hidden rounded-2xl border border-grey-700 bg-grey-800 p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle>Shop</DialogTitle>
+              <DialogDescription>
+                Here, you'll be able to buy characters with your primogens!
+              </DialogDescription>
+
+              <p>
+                <!-- Are you sure you want to deactivate your account? All of your data will
+                be permanently removed. This action cannot be undone. -->
+              </p>
+
+              <button @click="props.closeShopDialog">Close</button>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  DialogDescription,
+  TransitionRoot,
+  TransitionChild,
+} from "@headlessui/vue";
+
+const props = defineProps<{
+  open: boolean;
+  closeShopDialog: () => unknown;
+}>();
+</script>
